f62c1f2f22d1128b26491435962ce6846a8656fc
primes-banque-fr <tchammamadi99@gmail.com>
2026-05-25 14:50:45 +0100

    Update menu.py

--- handlers/menu.py
+++ handlers/menu.py
@@ -7,45 +7,44 @@ async def menu_handler(update, context):
     text = update.message.text
     user = update.message.from_user
 
-    # acheter
+    # 🛒 MENU ACHAT
     if text == "🛒 Acheter":
 
-        keyboard = [["CAMTEL", "MTN"]]
+        keyboard = [
+            ["⚡ CAMTEL", "🚀 MTN"]
+        ]
 
         await update.message.reply_text(
-            "Choisissez un produit",
+            "🛒💎 𝐂𝐇𝐎𝐈𝐒𝐈𝐒𝐒𝐄𝐙 𝐔𝐍 𝐏𝐑𝐎𝐃𝐔𝐈𝐓\n"
+            "━━━━━━━━━━━━━━━━",
             reply_markup=ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
         )
 
-    # produit sélectionné
-    elif text in ["CAMTEL", "MTN"]:
+    # ⚡ PRODUIT CAMTEL / MTN
+    elif text in ["⚡ CAMTEL", "🚀 MTN"]:
 
-        ref = f"HMB-{random.randint(10000,99999)}"
+        ref = f"HMB-{random.randint(10000, 99999)}"
 
         cursor.execute("""
-        INSERT INTO orders (user_id, username, product, plan, amount, status, ref)
-        VALUES (?, ?, ?, ?, ?, ?, ?)
+        INSERT INTO orders (user_id, username, product, plan, amount, status)
+        VALUES (?, ?, ?, ?, ?, ?)
         """, (
             user.id,
             user.username,
             text,
             "1 mois",
-            1000,
-            "pending",
-            ref
+            "1000F",
+            "pending"
         ))
 
         conn.commit()
 
         await update.message.reply_text(
-f"""
-💳 Paiement requis
-
-📦 Produit : {text}
-🔖 Référence : {ref}
-
-📲 MTN / Orange Money
-
-Envoyez la capture après paiement.
-"""
-      )
+            "💳✨ 𝐏𝐀𝐈𝐄𝐌𝐄𝐍𝐓 𝐑𝐄𝐐𝐔𝐈𝐒\n"
+            "━━━━━━━━━━━━━━━━\n"
+            f"📦 Produit : {text}\n"
+            f"🔖 Référence : {ref}\n"
+            "━━━━━━━━━━━━━━━━\n"
+            "📲 MTN / Orange Money\n"
+            "📸 Envoyez la capture après paiement"
+        )
