8897ab6049ebbafe1babfe2f7c0119309db5080c
primes-banque-fr <tchammamadi99@gmail.com>
2026-05-25 14:48:29 +0100

    Update callbacks.py

--- handlers/callbacks.py
+++ handlers/callbacks.py
@@ -1,16 +1,24 @@
 from handlers.catalog import catalog
 from handlers.camtel import camtel
 from handlers.payment import payment
+from handlers.start import start_cmd
 
 async def callback_router(update, context):
 
     data = update.callback_query.data
 
+    # 🛒 Catalogue
     if data == "catalog":
         return await catalog(update, context)
 
+    # ⚡ CAMTEL
     if data == "camtel":
         return await camtel(update, context)
 
+    # 💳 Paiement
     if data == "payment":
         return await payment(update, context)
+
+    # 🏠 Retour START
+    if data == "start":
+        return await start_cmd(update, context)
