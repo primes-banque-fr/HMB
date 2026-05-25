dc97b9b341274674d1e9e3f474f6a0f68de0e0ae
primes-banque-fr <tchammamadi99@gmail.com>
2026-05-25 14:03:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,22 @@
 import asyncio
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, CallbackQueryHandler, filters
+from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, MessageHandler, filters
 
 from config import BOT_TOKEN
 from database import init_db
 
 from handlers.start import start_cmd
-from handlers.menu import menu_handler
+from handlers.callbacks import callback_router
 from handlers.capture import handle_capture
-from handlers.admin import admin_callback
 
-async def run_bot():
+async def run():
 
     init_db()
 
     app = ApplicationBuilder().token(BOT_TOKEN).build()
 
     app.add_handler(CommandHandler("start", start_cmd))
-    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, menu_handler))
+    app.add_handler(CallbackQueryHandler(callback_router))
     app.add_handler(MessageHandler(filters.PHOTO, handle_capture))
-    app.add_handler(CallbackQueryHandler(admin_callback))
 
     print("BOT ONLINE")
 
@@ -26,11 +24,10 @@ async def run_bot():
     await app.start()
     await app.updater.start_polling()
 
-    # garder le bot vivant
     await asyncio.Event().wait()
 
 def main():
-    asyncio.run(run_bot())
+    asyncio.run(run())
 
 if __name__ == "__main__":
     main()
