d663a92a04566e6bbda95a6d4f8ea97472b1a7a6
primes-banque-fr <tchammamadi99@gmail.com>
2026-05-26 12:43:54 +0100

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID"))
+ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
+OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
 
-MTN_NUMBER = os.getenv("MTN_NUMBER")
-ORANGE_NUMBER = os.getenv("ORANGE_NUMBER")
+BOT_NAME = "HMB Support AI"
+CREATOR = "Le Roy HMB"
+
+ORANGE_MONEY = "692274053"
+MTN_MOMO = "651388771"
